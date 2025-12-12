e2f9972b913a16e71bc4369f57ec2d1327779ea4
Farjaz Kashif <farjazkashif01@gmail.com>
2025-12-12 23:45:05 +0500

    Commit...

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,6 +1,24 @@
 package main;
+
+import java.util.*;
+
 public class Main {
     public static void main(String[] args) {
+        int option = 0;
+        Scanner sc = new Scanner(System.in);
         
+        System.out.println("--------- Welcome to Hamdard Connect ---------");
+        System.out.println("1. Register");
+        System.out.println("2. Login");
+        System.out.println("3. Exit");
+        option = sc.nextInt();
+
+        switch(option) {
+             case 1:
+                 
+                 break;
+             default:
+                 throw new AssertionError();
+        }
     }
 }
